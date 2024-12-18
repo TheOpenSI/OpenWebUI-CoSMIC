94dd832a6ad63400af0b2b04622766e73152367a
Zhiwei (Danny) Xu <zhiwei.xu@canberra.edu.au>
2024-12-18 17:13:30 +1100

    Added a useful command to investigate a certain container

--- common_commands.sh
+++ common_commands.sh
@@ -1,5 +1,6 @@
 # Step in container and check folder structure.
 docker run -it open-webui /bin/bash
+docker exec -it [container_id] [linux command]
 
 # Remove docker images.
 docker rmi -f $(docker images -aq)
